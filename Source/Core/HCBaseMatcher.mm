5f83bf18820bccd334326a3c4ac613889b8f8ad7
Ayal Spitz <github.code@blackgecko.com>
2012-08-01 21:30:13 -0400

    Updated to work properly with Xcode 4.4

--- Source/Core/HCBaseMatcher.mm
+++ Source/Core/HCBaseMatcher.mm
@@ -22,7 +22,7 @@
 {
 	NSString *className = NSStringFromClass([self class]);
     [NSException raise:NSGenericException
-                format:@"-[%@  %s] not implemented", className, command];
+                format:@"-[%@  %@] not implemented", className, NSStringFromSelector(command)];
 }
 
 @end
